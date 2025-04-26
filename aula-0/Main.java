688bfb276f1b12e3f41c31f440612bc37404d51c
Henry-088 <henrycezar09@gmail.com>
2025-04-26 11:19:30 -0300

    Primeira atividade

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		Scanner entrada=new Scanner(System.in);
+		char henry='h';
+		int idade=15;
+		boolean bolacha= true;
+		int laranja= 2;
+		int uva= 5;
+		int soma= laranja+uva;
+		System.out.print("digite o valor da laranja:");
+		int ValorDaLaranja=entrada.nextInt();
+		System.out.print("digite o valor da uva:");
+		int ValorDaUva=entrada.nextInt();
+		System.out.print("Resultado da soma"+soma);
+	
+		
+
 	}
 }
