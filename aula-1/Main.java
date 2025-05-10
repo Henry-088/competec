e9047c653b665015bb1022442d23884f9c055de1
Henry-088 <henrycezar09@gmail.com>
2025-05-10 11:56:51 -0300

    aula 1.java

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		char henry = 'h';
+		char eduardo = 'e';
+		
+		System.out.println(henry!=eduardo);
+		System.out.println(false);
+		
+		
+		boolean chovendo = false;
+		boolean relampeando = true;
+		if (chovendo && relampeando){
+		    System.out.println("nao sairei");
+		}
+		else {
+		System.out.println("irei sair de casa");
+		}
+		boolean macarrao = true;
+		boolean strogonoff = false;
+		if (macarrao || strogonoff){
+		    System.out.println("ficarei muito feliz");
+		}
+	else {
+	System.out.println("não ficarei feliz");
 	}
+}
 }
