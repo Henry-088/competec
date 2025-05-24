f07d529bb923152c2ff91f5c62f3fcafec1e640d
Henry-088 <henrycezar09@gmail.com>
2025-05-24 11:32:12 -0300

    ativi 3

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		char nico = 'n';
+		int x =5;
+		
+		while(x < 10){
+		    System.out.println(nico);
+		   x++; 
+		   
+		   
+		 Scanner entrada = new Scanner(System.in);
+		 char primeiraLetra;
+		 int contador = 0;
+		 
+		 System.out.println("digite uma letra");
+		 primeiraletra= entrada .next().CharAt(0);
+		 while(contador < 5){
+		     system.out.println(primeira Letra);
+		     contador++;
+		     
+		     
+		    
+		 }
+		 
+		
+		}
+		
+		
+	
 	}
 }
+
